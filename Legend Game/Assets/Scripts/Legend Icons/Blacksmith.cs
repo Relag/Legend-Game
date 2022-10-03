2442f581fdde310c50f5538555755b504fa46812
Relag <rejean.lagasse93@gmail.com>
2022-10-02 21:22:31 -0400

    Typo fix
    Fixed a typo in Blacksmith.cs. Moved LegendIconList from Town to GameManager.

--- Legend Game/Assets/Scripts/Legend Icons/Blacksmith.cs
+++ Legend Game/Assets/Scripts/Legend Icons/Blacksmith.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blackasmith : MonoBehaviour
+public class Blacksmith : LegendIcons
 {
     // Start is called before the first frame update
     void Start()
--- Legend Game/Assets/Scripts/TownManager.cs
+++ Legend Game/Assets/Scripts/TownManager.cs
@@ -16,7 +16,6 @@ public class TownManager : MonoBehaviour
     int knowledgeUsed;
     int materialUsed;
 
-    List<LegendIcons> legendIcondList;
     
     // Start is called before the first frame update
     void Start()
