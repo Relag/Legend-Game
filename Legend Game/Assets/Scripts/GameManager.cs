644d52a43daf38261b4f42291f7a1d3989b885aa
Relag <rejean.lagasse93@gmail.com>
2022-10-02 22:59:34 -0400

    Imported TextmeshPro
    Imported TextMeshPro into the project. Continued UI development.

--- Legend Game/Assets/Scripts/GameManager.cs
+++ Legend Game/Assets/Scripts/GameManager.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    List<LegendIcons> legendIcondList;
+
+   //Blacksmith blacksmith;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Blacksmith blacksmith = new Blacksmith();
+        legendIcondList.Add(blacksmith);
     }
 
     // Update is called once per frame
